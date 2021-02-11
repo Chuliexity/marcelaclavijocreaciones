b085671c36955ea508da7bee7eb3bb242ad40bc6
Chuliexity <julietad.coder@gmail.com>
2021-02-11 11:51:29 -0300

    se agrega el resto de las carpetas

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  effect: 'fade',
+
+});
